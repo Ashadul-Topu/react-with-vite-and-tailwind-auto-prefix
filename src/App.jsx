f06549f754830d663d51a7a7aae6d84efb519de9
Ashadul-Topu <ashaduldemo@gmail.com>
2023-05-11 12:05:20 +0600

    React js with vite and tailwind css updat version

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+    <div className="h-[100vh] flex items-center justify-center">
+      <h1 className='text-3xl font-bold text-green-700 text-center'>React + vite setup file with tailwind and postcss</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
